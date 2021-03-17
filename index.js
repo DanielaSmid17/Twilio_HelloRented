4729fa524d53ee9bb296753c28444ecd8918e542
DanielaSmid17 <dani.smid@gmail.com>
2021-03-17 12:48:36 +0200

    port

--- index.js
+++ index.js
@@ -24,6 +24,10 @@ app.use('/voice/events', events)
 app.use('/voice/recordings', recordings)
 
 const port = process.env.PORT;
+const server = app.listen(port, function () {
+    console.log(`Express server listening on ${port}`, port, app.get('env'));
+  });
+
 const server = app.listen(port || 5000, () => {
     console.log(`Listening on port ${port}`);
 })
