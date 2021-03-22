ff83b327129057dc29ba78c3309d36b2513c8b26
DanielaSmid17 <dani.smid@gmail.com>
2021-03-22 12:51:18 +0200

    bug fixing

--- server.js
+++ server.js
@@ -6,6 +6,24 @@ require('dotenv').config();
 
 //require routes
 app.use(cors({origin:['http://localhost:8000', 'https://hr-twilio-fe.herokuapp.com']}))
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://hr-twilio-fe.herokuapp.com');
+
+  // Request methods you wish to allow
+  // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  // res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 const callIn = require("./routes/call-in")(app)
 const callOut = require("./routes/call-out")(app)
