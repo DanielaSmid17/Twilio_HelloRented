404907f0b0bacb8bd397f66f7758a3aac0441152
DanielaSmid17 <dani.smid@gmail.com>
2021-03-22 13:18:40 +0200

    bugs2

--- server.js
+++ server.js
@@ -5,24 +5,25 @@ const app = express()
 require('dotenv').config();
 
 //require routes
-app.use(cors({origin:['http://localhost:8000', 'https://hr-twilio-fe.herokuapp.com']}))
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
+// {origin:['http://localhost:8000', 'https://hr-twilio-fe.herokuapp.com']}
+app.use(cors())
+// app.use(function (req, res, next) {
+//   // Website you wish to allow to connect
+//   res.setHeader('Access-Control-Allow-Origin', '*');
 
-  // Request methods you wish to allow
-  // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//   // Request methods you wish to allow
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-  // Request headers you wish to allow
-  // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//   // Request headers you wish to allow
+//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  // res.setHeader('Access-Control-Allow-Credentials', true);
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader('Access-Control-Allow-Credentials', true);
 
-  // Pass to next layer of middleware
-  next();
-});
+//   // Pass to next layer of middleware
+//   next();
+// });
 
 const callIn = require("./routes/call-in")(app)
 const callOut = require("./routes/call-out")(app)
@@ -43,12 +44,12 @@ const server = app.listen(port, function () {
     console.log(`Express server listening on ${port}`, port, app.get('env'));
   });
 
-const io = require('socket.io')(server)
+const io = require('socket.io')(server,{cors:{origin:'*'}})
 io.on('connection', function(client){
   console.log('socket connected');
 })
 io.on("connect_error", (err) => {
   console.log(`connect_error due to ${err.message}`);
 });
-app.set("io", io)
+app.set("io", io) 
 
