cd94a2e84c7352f1f7f4e1fb4b7c6c3948d2a3b5
DanielaSmid17 <dani.smid@gmail.com>
2021-03-29 13:20:22 +0300

    cors

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 app.use(cors());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://hr-twilio-fe.herokuapp.com")})
+  res.header("Access-Control-Allow-Origin", "*")})
 
 //require routes
 const callIn = require("./routes/call-in")(app)
